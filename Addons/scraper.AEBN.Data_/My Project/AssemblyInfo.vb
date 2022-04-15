247833d710141ff16e222e4be482324c9d6eb537
justme75ac <103650506+justme75ac@users.noreply.github.com>
2022-04-15 14:06:30 +0200

    -

--- Addons/scraper.AEBN.Data_/My Project/AssemblyInfo.vb
+++ Addons/scraper.AEBN.Data_/My Project/AssemblyInfo.vb
@@ -8,17 +8,17 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("scraper.Data.MoviepilotDE")> 
+<Assembly: AssemblyTitle("scraper.Data.AEBN")> 
 <Assembly: AssemblyDescription("http://forum.xbmc.org/forumdisplay.php?fid=195")> 
 <Assembly: AssemblyCompany("Ember Media Manager")>
-<Assembly: AssemblyProduct("scraper.Data.MoviepilotDE")>
-<Assembly: AssemblyCopyright("Copyright © 2021")>
+<Assembly: AssemblyProduct("scraper.Data.AEBN")> 
+<Assembly: AssemblyCopyright("Copyright © 2013")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("40A61BA0-2503-4052-9F09-0C7A0B6C8601")>
+<Assembly: Guid("C57F3CA4-7640-48F8-89F7-6E9224740CEE")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.10.0.0")>
-<Assembly: AssemblyFileVersion("1.10.0.0")>
+<Assembly: AssemblyVersion("1.4.8.0")>
+<Assembly: AssemblyFileVersion("1.4.8.0")>
